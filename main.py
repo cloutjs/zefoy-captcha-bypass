7a152c8e09cac13bebadd2185f4bf72dba855aa1
clout <59515681+cloutjs@users.noreply.github.com>
2021-10-20 14:39:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 browser = webdriver.Chrome()
 browser.set_window_size(1920, 1080)
-browser.get("https://zefoy.com/ ")
+browser.get("https://zefoy.com/")
 browser.add_cookie({"name": "PHPSESSID", "value": "paste your ssid here"})
 browser.refresh()
 time.sleep(30)
